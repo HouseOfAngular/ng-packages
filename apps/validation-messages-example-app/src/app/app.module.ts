f16351f4f2b2fbe56cbba8473492fb5ebcedc9e1
Dawid Kostka <dawid.kostka@valueadd.pl>
2023-05-17 11:40:54 +0200

    feat(validation-messages): update example app

--- apps/validation-messages-example-app/src/app/app.module.ts
+++ apps/validation-messages-example-app/src/app/app.module.ts
@@ -4,10 +4,15 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { ValidationMessagesModule } from '../../../../projects/validation-messages/src';
+import {
+  ValidationMessagesConfig,
+  ValidationMessagesModule,
+  ValidationMessagesService,
+} from '../../../../projects/validation-messages/src';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,8 +23,28 @@ import { MatInputModule } from '@angular/material/input';
     BrowserAnimationsModule,
     ReactiveFormsModule,
     MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  validationMessagesConfig: ValidationMessagesConfig = {
+    required: 'Field is required',
+    email: 'Invalid email',
+    min: 'Value is too small: {{actual}}/{{min}}',
+    max: 'Value is too large {{actual}}/{{max}}',
+    minlength: 'Too short {{actualLength}}/{{requiredLength}}',
+    maxlength: 'Too long {{actualLength}}/{{requiredLength}}',
+    lettersPattern: {
+      message: 'Must contains only letters {{requiredPattern}}',
+      pattern: '^[a-zA-Z]*$',
+    },
+  };
+
+  constructor(private validationMessagesService: ValidationMessagesService) {
+    this.validationMessagesService.setValidationMessages(
+      this.validationMessagesConfig
+    );
+  }
+}

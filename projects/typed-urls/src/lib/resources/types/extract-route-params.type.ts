3fce4c8a7b0f6e6ad51f39e0062c962b6fb5981c
Dawid Kostka <dawid.kostka@valueadd.pl>
2023-05-19 13:51:30 +0200

    fix(typed-urls): Fix bug where passing urls with multiple params resulted in Record<string, string> type

--- projects/typed-urls/src/lib/resources/types/extract-route-params.type.ts
+++ projects/typed-urls/src/lib/resources/types/extract-route-params.type.ts
@@ -2,7 +2,7 @@ export type ExtractRouteParams<T> = string extends T
   ? Record<string, string>
   : /* eslint-disable-next-line */
   T extends `${infer _Start}/:${infer Param}/${infer Rest}`
-  ? { [k in Param | keyof ExtractRouteParams<Rest>]: string }
+  ? { [k in Param | keyof ExtractRouteParams<`/${Rest}`>]: string }
   : /* eslint-disable-next-line */
   T extends `${infer _Start}/:${infer Param}`
   ? { [k in Param]: string }

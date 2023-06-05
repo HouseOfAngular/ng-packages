f95158637def8a55aedd7cf346d0ee34cd09922d
Dawid Kostka <dawid.kostka@valueadd.pl>
2023-06-05 17:54:52 +0200

    fix(validation-messages): export utils from library, add merge config messages util to index.ts

--- projects/validation-messages/src/index.ts
+++ projects/validation-messages/src/index.ts
@@ -1,5 +1,5 @@
-
 export * from './lib/services';
 export * from './lib/components/validation-messages/validation-messages.component';
 export * from './lib/validation-messages.module';
 export * from './lib/resources';
+export * from './lib/utils';

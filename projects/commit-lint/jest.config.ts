d1e842da5e14112fa27bf77f36d26a4b33b8914d
Lukasz Myszkowski <lukasz.myszkowski@valueadd.pl>
2023-07-05 15:00:03 +0200

    fix: fix building commit-lint

--- projects/commit-lint/jest.config.ts
+++ projects/commit-lint/jest.config.ts
@@ -1,22 +1,10 @@
-/* eslint-disable */
+/* eslint-dis/* eslint-disable */
 export default {
   displayName: 'projects-commit-lint',
   preset: '../../jest.preset.cjs',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/projects/commit-lint',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': [
-      'jest-preset-angular',
-      {
-        tsconfig: '<rootDir>/tsconfig.spec.json',
-        stringifyContentPathRegex: '\\.(html|svg)$',
-      },
-    ],
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
-  snapshotSerializers: [
-    'jest-preset-angular/build/serializers/no-ng-attributes',
-    'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-comment',
-  ],
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/projects/commit-lint',
 };
